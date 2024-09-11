224c061de55c1c08be3d689d3a2b1371b38b9310
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-11 10:05:08 +0900

    artice_list/controller ì 수정 및 테스트게시글 추가

--- src/main/java/com/example/mission/MainController.java
+++ src/main/java/com/example/mission/MainController.java
@@ -11,4 +11,6 @@ public class MainController {
     public String article (){
         return "article list!!!!!!!!!";
     }
+
+
 }
--- src/test/java/com/example/mission/MissionApplicationTests.java
+++ src/test/java/com/example/mission/MissionApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.mission;
 
+import com.example.mission.Article.Article;
+import com.example.mission.Article.ArticleRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class MissionApplicationTests {
 
+	@Autowired
+	private ArticleRepository articleRepository;
+
 	@Test
 	void contextLoads() {
+		Article a = new Article();
+		a.setTitle("테스트용");
+		a.setContent("지문입니다.");
+		a.setCreateDate(LocalDateTime.now());
+		this.articleRepository.save(a);
+
 	}
+
 
 }
