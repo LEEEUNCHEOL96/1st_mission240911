b36d80d61e1e7a1e9a01d816cf15f6080aeae335
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-11 09:43:09 +0900

    article_list 생성

--- src/main/java/com/example/mission/Article/ArticleController.java
+++ src/main/java/com/example/mission/Article/ArticleController.java
@@ -3,15 +3,13 @@ package com.example.mission.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/article")
 @Controller
 public class ArticleController {
 
     @GetMapping("/list")
-    @ResponseBody
     public String articleList (){
-        return "article list";
+        return "article_list";
     }
 }
